26344a208ba481c65d8b88f426f1b42d0b6e84cf
mac501pl <42701371+mac501pl@users.noreply.github.com>
2018-10-19 14:16:44 +0200

    PIOlab2
    zmiany po drugim labolatorium

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,29 @@
 import java.util.Random;
+
 public class Player {
     private Random dye = new Random();
+    private String name = "Domyslne";
 
-    public int guess(){
-        return dye.nextInt(6)+1;
+    public Player() {
+        //    this.name = "Domyslne";   //bez deklaracji i bez domyślnej wartości będzie NULL, tak ponoć źle
+        //    setName("Domyslne");      //a może tak?
+    }
+
+    public Player(String name) {
+        setName(name);
+    }
+
+    public int guess() {
+        return dye.nextInt(6) + 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.isEmpty()) {  //albo "".equals(name); ale to źle
+            this.name = name;                   //ważne kolejność! najpierw sprawdzamy czy jest różne od NULL
+        }                                       //nie ma else bo name już ma wartość przypisaną - domyślną
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,18 +3,20 @@ import java.util.Random;
 public class main {
     public static void main(String[] args) {
         Random r = new Random();
-        Player p = new Player();
-        boolean end = false;
-        while(!end){
-            int x = r.nextInt(6)+1;
-            int y = p.guess();
-            if(x == y){
+        Player p = new Player("Janek");
+//        Player p = new Player();
+
+        //p.setName("Janek");
+        int x, y;
+        System.out.println(p.getName());
+        do {
+            x = r.nextInt(6) + 1;
+            y = p.guess();
+            if (x == y) {
                 System.out.println(x + " = " + y);
-                end = true;
-            }
-            else{
+            } else {
                 System.out.println(x + " != " + y);
             }
-        }
+        } while (x != y);
     }
 }
